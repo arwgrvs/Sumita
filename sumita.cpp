92fd48c849a24509049de7191f1b3ff7233b66b4
arwgrvs <goal080813hgtrgna2@cbtis217.edu.mx>
2024-10-16 08:36:27 -0600

    Comentarios nuevos :D

--- sumita.cpp
+++ sumita.cpp
@@ -3,11 +3,15 @@ using namespace std;
 
 int main()
 {
+    //olaa, esto es una sumita bien facil :D
     int a, b, c;
     cout<<"Asignar valores a 'a' y 'b'\n";
+    //Aqui se leen los valores
     cin>>a>>b;
+    //Aqui sale la suma
     c=a+b;
     cout<<"La suma es: "<<c<<"\n";
+    cout<<":D\n";
 
     return 0;
 }
